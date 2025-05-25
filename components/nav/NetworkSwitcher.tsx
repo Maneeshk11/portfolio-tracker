486e5ac70c2f13e6bcc2f5d46a049c0693a96230
Maneesh Kolli <maneesh.kolli@gmail.com>
2025-05-25 15:13:26 -0400

    fixed network select behaviour

--- components/nav/NetworkSwitcher.tsx
+++ components/nav/NetworkSwitcher.tsx
@@ -7,6 +7,10 @@ import {
   SelectValue,
 } from "../ui/select";
 import { useSwitchChain } from "wagmi";
+import { Button } from "../ui/button";
+import { Repeat2 } from "lucide-react";
+import { useState, useRef, useEffect } from "react";
+
 interface NetworkSwitcherProps {
   chainId: number;
 }
@@ -14,23 +18,52 @@ interface NetworkSwitcherProps {
 const NetworkSwitcher = ({ chainId }: NetworkSwitcherProps) => {
   const { switchChain, chains } = useSwitchChain();
   const { setChainId } = useAlchemy();
-  const chainSwitch = (chain: number) => {
-    if (chain === chainId) {
-      return;
+  const [value, setValue] = useState(chainId.toString());
+  const [open, setOpen] = useState(false);
+
+  const blockCloseRef = useRef(false);
+
+  useEffect(() => {
+    setValue(chainId.toString());
+  }, [chainId]);
+
+  const chainSwitch = (newChain: number) => {
+    setOpen(false);
+
+    if (newChain === chainId) return;
+    setChainId(newChain);
+    switchChain({ chainId: newChain as 1 | 11155111 });
+    setValue(newChain.toString());
+  };
+
+  const handleValueChange = (newValue: string) => {
+    blockCloseRef.current = newValue !== chainId.toString();
+    setValue(newValue);
+  };
+
+  const handleOpenChange = (nextOpen: boolean) => {
+    if (!nextOpen && blockCloseRef.current) {
+      blockCloseRef.current = false;
+      setOpen(true);
+    } else {
+      blockCloseRef.current = false;
+      setValue(chainId.toString());
+      setOpen(nextOpen);
     }
-    setChainId(chain);
-    switchChain({ chainId: chain as 1 | 11155111 });
   };
 
   return (
     <Select
-      defaultValue={chainId.toString()}
       name="network"
-      onValueChange={(value) => chainSwitch(Number(value))}
+      value={value}
+      onValueChange={handleValueChange}
+      open={open}
+      onOpenChange={handleOpenChange}
+      defaultValue={chainId.toString()}
     >
       <SelectTrigger
         size="sm"
-        className="text-gray-900 cursor-pointer border border-primary rounded-md px-4 py-2 font-medium  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 shadow-sm shadow-primary/20 transition-colors"
+        className="text-gray-900 w-32 cursor-pointer border border-primary rounded-md px-4 py-2 font-medium focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 shadow-sm shadow-primary/20 transition-colors"
       >
         <SelectValue placeholder="Select a network" />
       </SelectTrigger>
@@ -40,6 +73,19 @@ const NetworkSwitcher = ({ chainId }: NetworkSwitcherProps) => {
             {chain.name}
           </SelectItem>
         ))}
+
+        <div className="flex items-center gap-2 justify-center m-2">
+          <Button
+            variant="default"
+            size="sm"
+            disabled={value === chainId.toString()}
+            onClick={() => chainSwitch(Number(value))}
+            className="w-full text-xs font-medium cursor-pointer"
+          >
+            <Repeat2 className="w-4 h-4" />
+            Change
+          </Button>
+        </div>
       </SelectContent>
     </Select>
   );
