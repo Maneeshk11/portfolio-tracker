cf32581982228f34e95b6679c21037001547f81b
Maneesh Kolli <maneesh.kolli@gmail.com>
2025-05-30 19:41:04 -0400

    assets mapping

--- app/swap/page.tsx
+++ app/swap/page.tsx
@@ -1,56 +1,15 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Select, SelectTrigger, SelectValue } from "@/components/ui/select";
+"use client";
+
+import { PortfolioContextProvider } from "@/lib/contexts/usePortfolioState";
+import Swap from "./Swap";
 
 const SwapPage = () => {
   return (
-    <div className="flex justify-center items-center">
-      <Card className="w-lg">
-        <CardHeader>
-          <CardTitle>Swap</CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2 border border-primary/50 rounded p-4">
-            <span className="text-sm font-medium">Send</span>
-            <div className="flex gap-2 items-center">
-              <Input
-                className="shadow-none border-none bg-none focus-visible:ring-0 dark:bg-transparent p-0 text-xl dark:text-xl "
-                placeholder="0"
-              />
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a token" />
-                </SelectTrigger>
-              </Select>
-            </div>
-          </div>
-          <div className="flex flex-col gap-2 border border-primary/50 rounded p-4 ">
-            <span className="text-sm">Receive</span>
-            <div className="flex gap-2 items-center">
-              <Input
-                className="shadow-none border-none bg-none focus-visible:ring-0 dark:bg-transparent p-0 text-xl dark:text-xl"
-                placeholder="0"
-              />
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a token" />
-                </SelectTrigger>
-              </Select>
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter className="flex justify-center">
-          <Button className="w-24  cursor-pointer">Swap</Button>
-        </CardFooter>
-      </Card>
-    </div>
+    <PortfolioContextProvider>
+      <div className="flex justify-center items-center h-screen">
+        <Swap />
+      </div>
+    </PortfolioContextProvider>
   );
 };
 
