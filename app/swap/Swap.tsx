8c37898482878bbf19979b7b92780a3c69e49530
Maneesh Kolli <maneesh.kolli@gmail.com>
2025-06-02 19:45:18 -0400

    swap

--- app/swap/Swap.tsx
+++ app/swap/Swap.tsx
@@ -14,9 +14,25 @@ import { SelectValue } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { usePortfolioStore } from "@/lib/providers/portfolio-store-provider";
 import Image from "next/image";
+import { useState } from "react";
+import { Wallet } from "lucide-react";
+
+export type SwapAsset = {
+  address: string;
+  amount?: number;
+  symbol: string;
+  image?: string;
+  decimals?: number;
+  name?: string;
+  swapValue?: number;
+};
 
 const Swap = () => {
   const { assets } = usePortfolioStore((state) => state);
+
+  const [sendAsset, setSendAsset] = useState<SwapAsset | null>(null);
+  const [receiveAsset, setReceiveAsset] = useState<SwapAsset | null>(null);
+  const [invalidSwap, setInvalidSwap] = useState(false);
 
   return (
     <Card className="w-lg">
@@ -25,14 +41,42 @@ const Swap = () => {
       </CardHeader>
       <CardContent className="flex flex-col gap-4">
         <div className="flex flex-col gap-2 border rounded p-4 shadow-2xl shadow-primary/50 dark:shadow-primary/50">
-          <span className="text-sm font-medium">Send</span>
+          <div className="flex justify-between items-center">
+            <span className="text-sm font-medium">Send</span>
+            {sendAsset && (
+              <span className="text-sm font-medium flex items-center gap-2 text-gray-500">
+                <Wallet className="w-4 h-4" />
+                {sendAsset?.amount}
+              </span>
+            )}
+          </div>
           <div className="flex gap-2 items-center">
             <Input
-              className="shadow-none border-none bg-none focus-visible:ring-0 dark:bg-transparent p-0 text-xl dark:text-xl [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+              className={`${
+                invalidSwap ? "text-primary" : "text-gray-500"
+              } shadow-none border-none bg-none focus-visible:ring-0 dark:bg-transparent p-0 text-xl dark:text-xl [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none`}
               placeholder="0"
               type="number"
+              onChange={(e) => {
+                const value = e.target.value;
+                if (Number(value) > (sendAsset?.amount ?? 0)) {
+                  setInvalidSwap(true);
+                } else {
+                  setInvalidSwap(false);
+                }
+              }}
             />
-            <Select>
+            <Select
+              value={sendAsset?.address ?? ""}
+              onValueChange={(value) => {
+                const asset = assets.find((asset) => asset.address === value);
+                setSendAsset({
+                  address: asset?.address ?? "",
+                  symbol: asset?.symbol ?? "",
+                  amount: Number(asset?.amount),
+                });
+              }}
+            >
               <SelectTrigger className="w-40 bg-transparent dark:bg-transparent rounded-full">
                 <SelectValue placeholder="Select a token" />
               </SelectTrigger>
@@ -66,7 +110,17 @@ const Swap = () => {
               placeholder="0"
               type="number"
             />
-            <Select>
+            <Select
+              value={receiveAsset?.address ?? ""}
+              onValueChange={(value) => {
+                const asset = assets.find((asset) => asset.address === value);
+                setReceiveAsset({
+                  address: asset?.address ?? "",
+                  symbol: asset?.symbol ?? "",
+                  amount: Number(asset?.amount),
+                });
+              }}
+            >
               <SelectTrigger className="w-40 bg-transparent dark:bg-transparent rounded-full">
                 <SelectValue placeholder="Select a token" />
               </SelectTrigger>
